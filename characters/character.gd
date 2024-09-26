5d42291c53738455151f1713d1815705e6908885
Abel Roinsard <abelroinsard@gmail.com>
2024-09-26 12:39:25 +0200

    tilemap

--- characters/character.gd
+++ characters/character.gd
@@ -8,6 +8,8 @@ class_name Character
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+signal leave
 
 func _physics_process(delta: float) -> void:
 	return
+	
--- characters/character_platforming.gd
+++ characters/character_platforming.gd
@@ -8,12 +8,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if MultiplayerInput.is_action_just_pressed(player_index, "ui_accept") and is_on_floor():
+	if MultiplayerInput.is_action_just_pressed(player_index, "jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
