ca8a734b7e9771f62b6811467531bb6e9bcabe47
Abel Roinsard <abelroinsard@gmail.com>
2024-09-28 16:24:31 +0200

    feat: sprite for character left and right

--- characters/character_basic.gd
+++ characters/character_basic.gd
@@ -10,16 +10,18 @@ const JUMP_VELOCITY = -400.0
 var player: int
 var input
 
-# call this function when spawning this player to set up the input object based on the device
+enum Directions {
+	RIGHT,
+	LEFT
+}
+var sprite_direction : Directions
+
+@onready var sprite : Sprite2D = $Sprite2D
+
 func init(player_num: int, device: int):
 	player = player_num
-
-	# in my project, I got the device integer by accessing the singleton autoload PlayerManager
-	# but for simplicity, it's not an autoload in this demo.
-	# but I recommend making it a singleton so you can access the player data from anywhere.
-	# that would look like the following line, instead of the device function parameter above.
-	# var device = PlayerManager.get_player_device(player)
 	input = DeviceInput.new(device)
+	sprite_direction = Directions.RIGHT
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -27,15 +29,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	var direction = input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	# Arange sprite change
+	if input.is_action_just_pressed("left") and sprite_direction == Directions.RIGHT:
+		sprite_direction = Directions.LEFT
+	if input.is_action_just_pressed("right") and sprite_direction == Directions.LEFT:
+		sprite_direction = Directions.RIGHT
+	sprite.flip_h = sprite_direction
