d6b94eecb1a927f87515767ec9f3bab6de8ae110
Abel Roinsard <abelroinsard@gmail.com>
2024-09-28 15:56:15 +0200

    refactor: fuse the addon and the work already done

--- characters/character_platforming.gd
+++ characters/character_platforming.gd
@@ -1,14 +1,33 @@
-extends Character
+extends CharacterBody2D
 
 class_name CharacterPlatforming
 
+signal leave
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var player: int
+var input
+
+# call this function when spawning this player to set up the input object based on the device
+func init(player_num: int, device: int):
+	player = player_num
+
+	# in my project, I got the device integer by accessing the singleton autoload PlayerManager
+	# but for simplicity, it's not an autoload in this demo.
+	# but I recommend making it a singleton so you can access the player data from anywhere.
+	# that would look like the following line, instead of the device function parameter above.
+	# var device = PlayerManager.get_player_device(player)
+	input = DeviceInput.new(device)
+
 func _physics_process(delta: float) -> void:
-	super(delta)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	if MultiplayerInput.is_action_just_pressed(player_index, "jump") and is_on_floor():
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
