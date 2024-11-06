af65866a51307e1e2e1bb4e7e3761ed81c0c9ee0
Abel Roinsard <abelroinsard@gmail.com>
2024-11-06 13:35:55 +0100

    fix spring

--- characters/character_basic.gd
+++ characters/character_basic.gd
@@ -9,6 +9,7 @@ const JUMP_VELOCITY = -500.0
 
 var player: int
 var input
+var fan_vector: Vector2 = Vector2(0,0)
 
 enum Directions {
 	RIGHT,
@@ -17,13 +18,18 @@ enum Directions {
 var sprite_direction : Directions
 var is_squatting: bool
 
-
 @onready var sprite : Sprite2D = $Sprite2D
 
 func init(player_num: int, device: int):
 	player = player_num
 	input = DeviceInput.new(device)
 	sprite_direction = Directions.RIGHT
+
+func _ready() -> void:
+	Globals.player_characters[player] = self
+
+func apply_fan_force(force: Vector2) -> void:
+	fan_vector = force
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -45,18 +51,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		is_squatting = false
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = input.get_axis("left", "right")
+	velocity.x = 0
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x += direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x += move_toward(velocity.x, 0, SPEED)
+	velocity += fan_vector
 
+	# Apply the fan force
 	move_and_slide()
 	
-	# Arange sprite change
+	# Arrange sprite change
 	if input.is_action_just_pressed("left") and sprite_direction == Directions.RIGHT:
 		sprite_direction = Directions.LEFT
 	if input.is_action_just_pressed("right") and sprite_direction == Directions.LEFT:
--- props/Fan/fan.gd
+++ props/Fan/fan.gd
@@ -1,19 +1,21 @@
 extends StaticBody2D
 
-@export var STRENGTH = 100
-var vector : Vector2 = Vector2(-STRENGTH, 0)
+@export var STRENGTH = 10000
+var vector: Vector2 = Vector2(-STRENGTH, 0)
+var affected_players = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Sprite2D.play("default")
-	pass # Replace with function body.
-
+    $Sprite2D.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var bodies = $Area2D.get_overlapping_bodies()
-	for body in bodies:
-		if body is CharacterPlatforming: 
-			body.velocity += vector
-			body.move_and_slide()
-	pass
+    var bodies = $Area2D.get_overlapping_bodies()
+    affected_players = []
+    for body in bodies:
+        if body is CharacterPlatforming:
+            affected_players.append(body)
+            body.apply_fan_force(vector * delta)
+    for player in Globals.player_characters.values():
+        if player not in affected_players:
+            player.fan_vector = Vector2(0,0)
